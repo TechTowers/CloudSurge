697251e43e54b4c4f70454c4bacb017622257f2b
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-01-11 15:00:51 +0100

    feat/add-background-services: add initial daemon script

---
+++ scripts/cloudsurge-daemon.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Lists for costs
+PROVIDERS=()
+VMS=()
+LIMITS=()
+EXCEEDS=()
+
+# Fill the lists
+while read -r LINE; do
+  PROVIDERS+=("$(echo "$LINE" | cut -d ";" -f 1)")
+  VMS+=("$(echo "$LINE" | cut -d ";" -f 2)")
+  LIMITS+=("$(echo "$LINE" | cut -d ";" -f 3)")
+  EXCEEDS+=("$(echo "$LINE" | cut -d ";" -f 4)")
+done < <(flatpak run org.techtowers.CloudSurge -c)
+
+# Build the notification
+BODY=""
+
+for INDEX in $(seq ${#PROVIDERS[@]}); do
+  PROVIDER=${PROVIDERS[((INDEX - 1))]}
+  VM=${VMS[((INDEX - 1))]}
+  LIMIT=${LIMITS[((INDEX - 1))]}
+  EXCEED=${EXCEEDS[((INDEX - 1))]}
+  BODY+="$VM ($PROVIDER) exceeds the cost limit of $LIMIT by $EXCEED\n"
+done
+
+ACTION=$(
+  notify-send -a "CloudSurge" \
+    "One or more VMs exceeded the cost limit!" \
+    "$BODY" \
+    -u critical \
+    -A default="Open CloudSurge"
+)
+
+if [[ "$ACTION" == default ]]; then
+  flatpak run org.techtowers.CloudSurge
+fi
+
+if [[ $(flatpak run org.techtowers.CloudSurge -o) != "0" ]] && ! pgrep gns; then
+  ACTION=$(
+    notify-send -a "CloudSurge" \
+      "$VMS_RUNNING VMs are running!" \
+      "GNS3 Client does not seem to run locally." \
+      -u critical \
+      -A default="Open CloudSurge"
+  )
+
+  if [[ "$ACTION" == default ]]; then
+    flatpak run org.techtowers.CloudSurge
+  fi
+fi
