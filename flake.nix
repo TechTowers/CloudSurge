59af98bd48c5bbacfb0e5d9082ca84040ae714eb
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-01-12 13:42:49 +0100

    add nix dev environment

---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixpkgs-python = {
+      url = "github:cachix/nixpkgs-python";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+      devenv-test = self.devShells.${system}.default.config.test;
+    });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = with pkgs; [
+                git # duh
+                helix # the best code editor
+
+                gnome-builder
+                meson
+                flatpak
+                flatpak-builder
+              ];
+
+              languages.python = {
+                enable = true;
+                version = "3.12";
+                poetry = {
+                  enable = true;
+                  activate.enable = true;
+                  install.enable = true;
+                };
+              };
+
+              enterShell = ''
+                git --version
+                hx --version
+              '';
+
+              pre-commit.hooks = {
+                autoflake.enable = true;
+                ruff.enable = true;
+              };
+
+              env.PYTHONPATH = ./.;
+            }
+          ];
+        };
+      });
+  };
+}
